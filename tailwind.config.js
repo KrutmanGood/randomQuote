b44fb0ea726ae67c90a5bdaa623ba72287f24695
KrutmanGood <83672816+KrutmanGood@users.noreply.github.com>
2023-02-21 03:49:18 +0300

    Небольшие поправки

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,8 @@ module.exports = {
   content: ['./**/*.html'],
   theme: {
     extend: {
-      animation: {
-        typing: `animation: typing 3s step(20) 1`
-      },
-      keyframes: {
-        typing: {
-          from: {
-            width: '0',
-          },
-          to: {
-            width: '100%'
-          }
-        }
+      screens: {
+        smartphone: '414px'
       }
     },
   },
